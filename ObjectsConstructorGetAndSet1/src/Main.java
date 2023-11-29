2ffb404aac3e927504a7906ac2df657981f3f924
floralgreen <90903205+floralgreen@users.noreply.github.com>
2023-11-29 11:53:05 +0100

    Add files via upload

---
+++ ObjectsConstructorGetAndSet1/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * Scrivere un programma che contenga una classe che definisce uno Studente che abbia come proprietà nome, cognome ed identificativo.
+ * Per questa classe saranno necessari i metodi accessori (getters e setters) ed un costruttore.
+ * Nel main dichiarare un oggetto di tipo Studente.
+ */
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Studente studente1 = new Studente("Federico", "Carducci", "CRDFRC26000AAAA5");
+
+        System.out.println(studente1);
+
+    }
+
+}
---
+++ ObjectsConstructorGetAndSet1/src/Studente.java
@@ -0,0 +1,46 @@
+public class Studente {
+
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+
+
+    public Studente(String nome, String cognome, String codiceFiscale){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    @Override
+    public String toString() {
+        return "Studente{" +
+                "nome = " + nome +
+                ", cognome = " + cognome +
+                ", codiceFiscale = " + codiceFiscale +
+                '}';
+    }
+}
